743350de0f50bd1a2f53895677e6d9d499e15ce2
Jeff McCormick <jeff.mccormick@crunchydata.com>
2016-08-03 11:44:46 -0400

    update pgadmin4 example

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -14,7 +14,7 @@ sudo yum -y install net-tools bind-utils wget unzip git golang
 #
 # download the pgadmin4 python wheel distro
 #
-wget -O $BUILDBASE https://ftp.postgresql.org/pub/pgadmin3/pgadmin4/v1.0-beta3/pip/pgadmin4-1.0b3-py2-none-any.whl
+wget https://ftp.postgresql.org/pub/pgadmin3/pgadmin4/v1.0-beta3/pip/pgadmin4-1.0b3-py2-none-any.whl
 #
 # download the metrics products, only required to build the containers
 #
