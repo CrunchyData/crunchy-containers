8a4289a19de479f262226749a23c4fd229e122be
Jeff McCormick <jeff.mccormick@crunchydata.com>
2016-10-24 11:33:16 -0400

    update pgpool to better support external config files and add example of usage

--- bin/pgpool/startpgpool.sh
+++ bin/pgpool/startpgpool.sh
@@ -21,29 +21,24 @@ env
 
 BINDIR=/opt/cpm/bin
 CONFDIR=/opt/cpm/conf/pgpool
+CONFIGS=/tmp
 
+# seed with defaults included in the container image, this is the
+# case when /pgconf is not specified
 cp $CONFDIR/* /tmp
 
-function check_for_overrides() {
-        if [ -f /pgconf/pgpool.conf ]; then
-                echo "pgconf pgpool.conf is being used"
-		cp /pgconf/pgpool.conf /tmp
-        fi
-        if [ -f /pgconf/pool_hba.conf ]; then
-                echo "pgconf pool_hba.conf is being used"
-		cp /pgconf/pool_hba.conf /tmp
-        fi
-}
+if [ -f /pgconf/pgpool.conf ]; then
+	echo "pgconf pgpool.conf is being used"
+	CONFIGS=/pgconf
+fi
 
 # populate template with env vars
-sed -i "s/PG_MASTER_SERVICE_NAME/$PG_MASTER_SERVICE_NAME/g" /tmp/pgpool.conf
-sed -i "s/PG_SLAVE_SERVICE_NAME/$PG_SLAVE_SERVICE_NAME/g" /tmp/pgpool.conf
-sed -i "s/PG_USERNAME/$PG_USERNAME/g" /tmp/pgpool.conf
-sed -i "s/PG_PASSWORD/$PG_PASSWORD/g" /tmp/pgpool.conf
+sed -i "s/PG_MASTER_SERVICE_NAME/$PG_MASTER_SERVICE_NAME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_SLAVE_SERVICE_NAME/$PG_SLAVE_SERVICE_NAME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_USERNAME/$PG_USERNAME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_PASSWORD/$PG_PASSWORD/g" $CONFIGS/pgpool.conf
 
 # populate pool_passwd file
-/bin/pg_md5 --md5auth --username=$PG_USERNAME --config=/tmp/pgpool.conf $PG_PASSWORD
+/bin/pg_md5 --md5auth --username=$PG_USERNAME --config=$CONFIGS/pgpool.conf $PG_PASSWORD
 
-check_for_overrides
-
-/bin/pgpool -n -a /tmp/pool_hba.conf -f /tmp/pgpool.conf 
+/bin/pgpool -n -a $CONFIGS/pool_hba.conf -f $CONFIGS/pgpool.conf 
