bf0a4848121e8397047ce9a1151941acd9404cdb
Jeff McCormick <jeff.mccormick@crunchydata.com>
2016-11-01 12:27:06 -0400

    update openshift with pgbackrest example and update the Dockerfile for postgres using rhel7 to include pgbackrest packages

---
+++ examples/openshift/backrest/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2016 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source $BUILDBASE/examples/envvars.sh
+
+sudo mkdir /nfsfileshare/pgconf /nfsfileshare/backrestrepo
+sudo chown -R postgres:postgres  /nfsfileshare/pgconf /nfsfileshare/backrestrepo
+sudo cp ./pgbackrest.conf /nfsfileshare/pgconf
+
+envsubst <  backrestrepo-nfs-pv.json | oc create -f -
+oc create -f backrestrepo-nfs-pvc.json
+
+envsubst <  pgconf-nfs-pv.json | oc create -f -
+oc create -f pgconf-nfs-pvc.json
+
+oc process -f master-pod.json -v CCP_IMAGE_TAG=$CCP_IMAGE_TAG | oc create -f -
+
