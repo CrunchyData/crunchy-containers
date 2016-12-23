f85cfe0c726c13e9a11ae757e6c165c10d1e03d2
Jeff McCormick <jeff.mccormick@crunchydata.com>
2016-12-23 15:18:04 -0500

    fix openshift sync ...9.6 apparently doesnt like a dash in the sync host name

--- examples/openshift/sync/cleanup.sh
+++ examples/openshift/sync/cleanup.sh
@@ -16,6 +16,6 @@ oc delete service master
 oc delete service replica
 oc delete pod master
 oc delete pod replica
-oc delete pod sync-replica
+oc delete pod syncreplica
 $BUILDBASE/examples/waitforterm.sh master oc
 $BUILDBASE/examples/waitforterm.sh replica oc
