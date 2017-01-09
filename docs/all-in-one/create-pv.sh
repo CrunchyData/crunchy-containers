18efc732b1d12557adc56212c068512aeb2c4ab7
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-01-09 14:18:13 -0600

    update all-in-one setup with pv create scripts

---
+++ docs/all-in-one/create-pv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2016 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source $BUILDBASE/examples/envvars.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+envsubst < $DIR/nfs-pv-01.json |  oc create -f -
+envsubst < $DIR/nfs-pv-02.json |  oc create -f -
+envsubst < $DIR/nfs-pv-03.json |  oc create -f -
+envsubst < $DIR/nfs-pv-04.json |  oc create -f -
+envsubst < $DIR/nfs-pv-05.json |  oc create -f -
+envsubst < $DIR/nfs-pv-06.json |  oc create -f -
+envsubst < $DIR/nfs-pv-07.json |  oc create -f -
+envsubst < $DIR/nfs-pv-08.json |  oc create -f -
+envsubst < $DIR/nfs-pv-09.json |  oc create -f -
+envsubst < $DIR/nfs-pv-10.json |  oc create -f -
