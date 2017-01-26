8d8eee31e04a1c2eddeba02003e82262a0a75e6c
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-01-26 10:43:48 -0500

    update envvars script to not force hostname in /etc/hosts

--- examples/envvars.sh
+++ examples/envvars.sh
@@ -17,13 +17,6 @@
 # set up some env vars that all examples can relate to
 #
 
-HOSTNAME=`hostname`
-grep $HOSTNAME /etc/hosts
-if [ $? -ne 0 ]; then
-	echo "hostname is not in /etc/hosts which is required for the examples"
-	exit 1
-fi
-
 export LOCAL_IP=`hostname --ip-address`
 
 if [ -v $CCP_IMAGE_TAG ]; then
