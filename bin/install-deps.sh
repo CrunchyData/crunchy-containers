cbea617a576fc6e360effa0bef2d944b731f574a
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-03-30 12:44:15 -0500

    add license txt to install script

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# Copyright 2017 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 #
@@ -11,10 +25,6 @@ sudo yum -y install postgresql95-server
 
 sudo yum -y install net-tools bind-utils wget unzip git golang
 
-#
-# download the pgadmin4 python wheel distro
-#
-#wget https://ftp.postgresql.org/pub/pgadmin3/pgadmin4/v1.1/pip/pgadmin4-1.1-py2-none-any.whl
 #
 # download the metrics products, only required to build the containers
 #
