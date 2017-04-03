6a8142f36accf32a7a91856baa75fb02f839038f
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-04-03 08:58:15 -0500

    add initial dedicated templates

---
+++ examples/dedicated/create-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2017 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source $CCPROOT/examples/envvars.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$DIR/crunchy-backup/run.sh
+$DIR/crunchy-master/run.sh
+$DIR/crunchy-pgpool/run.sh
+$DIR/crunchy-proxy/run.sh
+$DIR/crunchy-replica/run.sh
+$DIR/crunchy-restore/run.sh
+$DIR/crunchy-sync-replica/run.sh
+
