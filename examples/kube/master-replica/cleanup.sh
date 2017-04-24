8cbdedc08fa2d97a5d383cba7041bd1db65af52b
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-04-24 16:13:13 -0500

    fix kube replica examples to shutdown in a more clean way

--- examples/kube/master-replica/cleanup.sh
+++ examples/kube/master-replica/cleanup.sh
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-kubectl delete service ms-master
-kubectl delete service ms-replica
-kubectl delete pod ms-master
 kubectl delete pod ms-replica
 kubectl delete pod ms-replica-2
+sleep  2
+kubectl delete service ms-replica
+kubectl delete service ms-master
+kubectl delete pod ms-master
 $CCPROOT/examples/waitforterm.sh ms-master kubectl
 $CCPROOT/examples/waitforterm.sh ms-replica kubectl
 $CCPROOT/examples/waitforterm.sh ms-replica-2 kubectl
