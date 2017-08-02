968cbf7110b162d57ba56f97a0ba7f466bdad24a
xenophenes <sarahwiz210@yahoo.com>
2017-08-02 11:09:36 -0700

    bump from 1.4.1 to 1.5, bump oc client from 1.4.1 to 1.5

--- examples/envvars.sh
+++ examples/envvars.sh
@@ -31,7 +31,7 @@ export NFS_IP=`hostname --ip-address`
 export LOCAL_IP=`hostname --ip-address`
 
 if [ -v $CCP_IMAGE_TAG ]; then
-	export CCP_IMAGE_TAG=rhel7-9.6-1.4.1
+	export CCP_IMAGE_TAG=rhel7-9.6-1.5
 	echo "CCP_IMAGE_TAG was not found...using current tag of " $CCP_IMAGE_TAG
 fi
 
