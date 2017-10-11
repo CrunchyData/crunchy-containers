91feadfc93579547ebb75a401450050d2aab126a
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-10-11 16:30:10 -0400

    fix openshift examples for CCP_IMAGE_PREFIX

--- examples/openshift/pgpool/run.sh
+++ examples/openshift/pgpool/run.sh
@@ -23,4 +23,4 @@ $DIR/cleanup.sh
 sudo mkdir $PV_PATH/pgpoolconfigdir
 sudo chown 7778 $PV_PATH/pgpoolconfigdir
 
-oc process -f $DIR/pgpool-rc.json -p CCP_IMAGE_TAG=$CCP_IMAGE_TAG | oc create -f -
+oc process -f $DIR/pgpool-rc.json -p CCP_IMAGE_PREFIX=$CCP_IMAGE_PREFIX CCP_IMAGE_TAG=$CCP_IMAGE_TAG | oc create -f -
