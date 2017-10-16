2e6b6e434a673aacf32c17e0de70a398a9e79627
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-10-16 11:48:14 -0400

    add CCP_IMAGE_PREFIX to examples

--- examples/openshift/metrics/run-pvc.sh
+++ examples/openshift/metrics/run-pvc.sh
@@ -24,4 +24,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/cleanup.sh
 
-oc process -f $DIR/metrics-pvc -p CCP_IMAGE_TAG=$CCP_IMAGE_TAG | oc create -f -
+oc process -f $DIR/metrics-pvc -p CCP_IMAGE_PREFIX=$CCP_IMAGE_PREFIX CCP_IMAGE_TAG=$CCP_IMAGE_TAG | oc create -f -
