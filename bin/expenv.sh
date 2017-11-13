20fd0075900799ea5357595a45fe05ebaf60e88f
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-11-13 08:41:59 -0600

    update expenv.sh script to be simpler

--- bin/expenv.sh
+++ bin/expenv.sh
@@ -15,44 +15,5 @@
 
 # Usage
 #$ cat inputfile | expenv.sh > outputfile
-#$ expenv.sh -f inputfile > outputfile
-#$ expenv.sh < inputfile > outputfile
-#$ expenv.sh -i -f inputfile // Replace inplace
 
-while getopts ":if:" opt; do
-  case $opt in
-    f)
-      useFile=$OPTARG
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
-    i)
-      inPlace=true
-      ;;
-  esac
-done
-
-if [ -n "$inPlace" ] && [ -z "$useFile" ]; then
- echo "Error: Option -i depends on option -f" >&2
-fi
-
-if [ -n "$inPlace" ]; then 
-  tmpFile=`mktemp`
-fi
-
-# Eval each line and redirect to tmpFile if set, otherwise to process stdout
-while read -r line; do
-  eval "echo $line" >> "${tmpFile:-/proc/${$}/fd/1}"
-done < "${useFile:-/proc/${$}/fd/0}"
-
-# Overwrite file
-if [ -n "$inPlace" ]; then 
-  mv -- $tmpFile $useFile
-fi
-
+while read -r line; do eval "echo $line"; done
