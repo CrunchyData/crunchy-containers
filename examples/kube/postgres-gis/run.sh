823c722c28eafe9b90a612d3dd92d4942b8f6f43
Steve Hetzel <steve-hetzel@users.noreply.github.com>
2017-12-12 16:29:17 -0700

    Adding echo statement to K8s postgres-gis example

--- examples/kube/postgres-gis/run.sh
+++ examples/kube/postgres-gis/run.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+echo "starting postgres-gis container..."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
