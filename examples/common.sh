98d018491b2decb2fc8b92dd61150a1f9976a375
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2018-07-19 17:22:38 -0400

    Update statefulset examples and storage creation

--- examples/common.sh
+++ examples/common.sh
@@ -38,14 +38,14 @@ function env_check_err() {
 }
 
 function dir_check_rm() {
-    if [[ -d ${CCP_STORAGE_PATH?}/${1?} ]]
+    if [[ -d ${CCP_STORAGE_PATH}/${1?} ]]
     then
         sudo rm -rf ${CCP_STORAGE_PATH?}/${1?} && echo_info "Deleted ${1?} from the data directory." || echo_err "${1?} was not successfully deleted from the data directory."
     fi
 }
 
 function file_check_rm() {
-    if [[ -f ${CCP_STORAGE_PATH?}/${1?} ]]
+    if [[ -f ${CCP_STORAGE_PATH}/${1?} ]]
     then
         sudo rm -f ${CCP_STORAGE_PATH?}/${1?} && echo_info "Deleted ${1?} from the data directory." || echo_err "${1?} was not successfully deleted from the data directory."
     fi
@@ -54,20 +54,28 @@ function file_check_rm() {
 function create_storage {
     env_check_err "CCP_STORAGE_CAPACITY"
     env_check_err "CCP_STORAGE_MODE"
+    PVC="${1?}-pvc.json"
 
-    if [ ! -z "$CCP_STORAGE_CLASS" ]; then
+    if [[ ! -z ${CCP_STORAGE_CLASS} ]]
+    then
         echo_info "CCP_STORAGE_CLASS is set. Using the existing storage class for the PV."
-        expenv -f $DIR/$1-pvc-sc.json | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
-        echo_info "Creating the example components.."
-    elif [ ! -z "$CCP_NFS_IP" ]; then
+        PVC="${1?}-pvc-sc.json"
+    elif [[ ! -z ${CCP_NFS_IP} ]]
+    then
         echo_info "CCP_NFS_IP is set. Creating NFS based storage volumes."
-        expenv -f $DIR/$1-pv-nfs.json | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
-        expenv -f $DIR/$1-pvc.json | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
-        echo_info "Creating the example components.."
+        PV="${1?}-pv-nfs.json"
     else
         echo_info "CCP_NFS_IP and CCP_STORAGE_CLASS not set. Creating HostPath based storage volumes."
-        expenv -f $DIR/$1-pv.json | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
-        expenv -f $DIR/$1-pvc.json | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
-        echo_info "Creating the example components.."
+        PV="${1?}-pv.json"
+    fi
+
+    if [[ -f ${DIR?}/${PV:-none} ]]
+    then
+       expenv -f ${DIR?}/${PV?} | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
+    fi
+
+    if [[ -f ${DIR?}/${PVC?} ]]
+    then
+       expenv -f ${DIR?}/${PVC?} | ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} -f -
     fi
 }
