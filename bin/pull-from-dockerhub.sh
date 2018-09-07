63e63399ac289abe8f855036d2775b96ba4c599b
Sarah Conway <sarah.conway@crunchydata.com>
2018-09-07 13:06:57 -0700

    deprecate crunchy-watch crunchy-containers-test issue 63

--- bin/pull-from-dockerhub.sh
+++ bin/pull-from-dockerhub.sh
@@ -30,7 +30,6 @@ docker pull $CCP_IMAGE_PREFIX/crunchy-grafana:$CCP_IMAGE_TAG
 docker pull $CCP_IMAGE_PREFIX/crunchy-collect:$CCP_IMAGE_TAG
 docker pull $CCP_IMAGE_PREFIX/crunchy-pgbadger:$CCP_IMAGE_TAG
 docker pull $CCP_IMAGE_PREFIX/crunchy-pgpool:$CCP_IMAGE_TAG
-docker pull $CCP_IMAGE_PREFIX/crunchy-watch:$CCP_IMAGE_TAG
 docker pull $CCP_IMAGE_PREFIX/crunchy-backup:$CCP_IMAGE_TAG
 docker pull $CCP_IMAGE_PREFIX/crunchy-postgres:$CCP_IMAGE_TAG
 docker pull $CCP_IMAGE_PREFIX/crunchy-postgres-gis:$CCP_IMAGE_TAG
