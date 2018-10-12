e1ab5c10664fd72ad83a635756a9878b7bcfa60c
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2018-10-12 13:41:16 -0400

    Add expenv to scheduler template

--- examples/kube/scheduler/add-schedules.sh
+++ examples/kube/scheduler/add-schedules.sh
@@ -4,12 +4,14 @@ source ${CCPROOT}/examples/common.sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+expenv -f ${DIR?}/configs/schedule-pgbasebackup.json > /tmp/schedule-pgbasebackup.json
+
 ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} configmap backrest-schedule-full \
     --from-file=${DIR?}/configs/schedule-backrest-full.json
 ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} configmap backrest-schedule-diff \
     --from-file=${DIR?}/configs/schedule-backrest-diff.json
 ${CCP_CLI?} create --namespace=${CCP_NAMESPACE?} configmap pgbasebackup-backup \
-    --from-file=${DIR?}/configs/schedule-pgbasebackup.json
+    --from-file=/tmp/schedule-pgbasebackup.json
 
 ${CCP_CLI?} label --namespace=${CCP_NAMESPACE?} configmap backrest-schedule-full crunchy-scheduler=true
 ${CCP_CLI?} label --namespace=${CCP_NAMESPACE?} configmap backrest-schedule-diff crunchy-scheduler=true
