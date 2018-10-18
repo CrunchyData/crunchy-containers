9384ff294c2a152b1522bf7ef6d409d59c5a4561
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2018-10-18 13:55:20 -0400

    Remove shard allocation from EFK example

--- examples/kube/centralized-logging/efk/run.sh
+++ examples/kube/centralized-logging/efk/run.sh
@@ -22,16 +22,3 @@ ${CCP_CLI?} create -f ${DIR?}/elasticsearch-statefulset.yaml
 ${CCP_CLI?} create -f ${DIR?}/fluentd-configmap.yaml
 ${CCP_CLI?} create -f ${DIR?}/fluentd-daemonset.yaml
 ${CCP_CLI?} create -f ${DIR?}/kibana-deployment.yaml
-
-echo_info "Sleeping until EFK stack is ready.."
-sleep 45
-
-# Replicate shards to all hosts
-URL='http://localhost:9200'
-${CCP_CLI?} exec -ti elasticsearch-logging-0 -n kube-system \
-  -- curl -XPUT "${URL?}/_cluster/settings" -H 'Content-Type: application/json' -d'
-{
-    "transient": {
-        "cluster.routing.allocation.enable": "all"
-    }
-}'
