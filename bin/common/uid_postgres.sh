94bf71c37408a9c6b73b86231444f69e2582c7db
ScalaEnthusiast <carvel.baus@crunchydata.com>
2018-10-19 16:32:02 -0400

    Fix for missing entrypoint in backrest restore; fix pgdump example execution; fix issues with password and group files

--- bin/common/uid_postgres.sh
+++ bin/common/uid_postgres.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-postgres}:x:$(id -u):0:${USER_NAME:-postgres} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-postgres}:x:$(id -u):26:${USER_NAME:-postgres} user:${HOME}:/bin/bash" >> /etc/passwd
+  fi
+  
+  if [ -w /etc/group ]; then
+    echo "nfsnobody:x:65534:" >> /etc/group
+    echo "postgres:x:$(id -u):postgres" >> /etc/group
   fi
 fi
 exec "$@"
+ 
