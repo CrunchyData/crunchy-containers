0091a4c86d9bb8198e2b59ef4e4d9c5278441bef
ScalaEnthusiast <carvel.baus@crunchydata.com>
2018-10-25 14:20:35 -0400

    Fix duplicate entry in password; correct install order on backrest

--- bin/common/uid_postgres.sh
+++ bin/common/uid_postgres.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-postgres}:x:$(id -u):26:${USER_NAME:-postgres} user:${HOME}:/bin/bash" >> /etc/passwd
+  	sed  "/postgres:x:26/d" /etc/passwd >> /tmp/uid.tmp
+  	cp /tmp/uid.tmp /etc/passwd
+  	rm -f /tmp/uid.tmp
+    echo "${USER_NAME:-postgres}:x:$(id -u):0:${USER_NAME:-postgres} user:${HOME}:/bin/bash" >> /etc/passwd
   fi
   
   if [ -w /etc/group ]; then
