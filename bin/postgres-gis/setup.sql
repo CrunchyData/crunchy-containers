31e455e3cdc47c01d32ce0891e5487c5be93153f
jmckulk <joseph.mckulka@crunchydata.com>
2019-09-26 14:37:52 -0400

    adds double quotes around username to allow for all usernames

--- bin/postgres-gis/setup.sql
+++ bin/postgres-gis/setup.sql
@@ -11,14 +11,14 @@ create extension plr;
 alter user postgres password 'PG_ROOT_PASSWORD';
 
 create user PG_PRIMARY_USER with REPLICATION  PASSWORD 'PG_PRIMARY_PASSWORD';
-create user PG_USER with password 'PG_PASSWORD';
+create user "PG_USER" with password 'PG_PASSWORD';
 
 create table primarytable (key varchar(20), value varchar(20));
 grant all on primarytable to PG_PRIMARY_USER;
 
 create database PG_DATABASE;
 
-grant all privileges on database PG_DATABASE to PG_USER;
+grant all privileges on database PG_DATABASE to "PG_USER";
 
 \c PG_DATABASE
 
@@ -30,11 +30,11 @@ create extension pg_stat_statements;
 create extension pgaudit;
 create extension plr;
 
-\c PG_DATABASE PG_USER;
+\c PG_DATABASE "PG_USER";
 
-create schema PG_USER;
+create schema "PG_USER";
 
-create table PG_USER.testtable (
+create table "PG_USER".testtable (
 	name varchar(30) primary key,
 	value varchar(50) not null,
 	updatedt timestamp not null
@@ -42,7 +42,7 @@ create table PG_USER.testtable (
 
 
 
-insert into PG_USER.testtable (name, value, updatedt) values ('CPU', '256', now());
-insert into PG_USER.testtable (name, value, updatedt) values ('MEM', '512m', now());
+insert into "PG_USER".testtable (name, value, updatedt) values ('CPU', '256', now());
+insert into "PG_USER".testtable (name, value, updatedt) values ('MEM', '512m', now());
 
-grant all on PG_USER.testtable to PG_PRIMARY_USER;
+grant all on "PG_USER".testtable to PG_PRIMARY_USER;
