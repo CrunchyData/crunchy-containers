dd6f2cf9e194d917a28f40de05525ae4c5657ad8
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2019-10-04 14:09:43 +0000

    Updated the 'crunchy-postgres', 'crunchy-collect' and 'crunchy-grafana' containers as needed to support Grafana v6.3.4 and pgMonitor v3.2.
    For the 'crunchy-grafana' container, this includes
    creating the 'notifiers' directory during container
    initialization to avoid a missing 'notifiers' directory
    error during Grafana startup.
    
    For the 'crunchy-collect' container, this includes
    ensuring the PGBACKREST_INFO_THROTTLE_MINUTES
    env var is properly set in the PostgreSQL Exporter
    queries file.
    
    For the 'crunchy-postgres' container, this includes
    creating and leveraging a custom shell script that
    can be utilized to obtain pgBackRest info, and
    ensuring the setup SQL file run during initialization
    of the pgMonitor module is properly configured to
    use the custom script.
    
    Additionally, the metrics example has been updated
    to remove port 9100 from the crunchy-collect
    containers.

--- bin/postgres/modules/pgmonitor.sh
+++ bin/postgres/modules/pgmonitor.sh
@@ -17,6 +17,9 @@ if [[ -v PGMONITOR_PASSWORD ]]
 then
     if [[ ${PG_MODE?} == "primary" ]] || [[ ${PG_MODE?} == "master" ]]
     then
+
+        echo_info "PGMONITOR_PASSWORD detected.  Enabling pgMonitor support."
+
         source /opt/cpm/bin/common_lib.sh
         export PGHOST="${PGHOST:-/tmp}"
 
@@ -40,17 +43,21 @@ then
             exit 1
         fi
 
+        echo_info "Using setup file '${function_file}' for pgMonitor"
+        cp "${function_file}" "/tmp/setup_pg.sql"
+        sed -i "s/\/usr\/bin\/pgbackrest-info.sh/\/opt\/cpm\/bin\/pgbackrest_info.sh/g" "/tmp/setup_pg.sql"
+
         # TODO Add ON_ERROR_STOP and single transaction when
         # upstream pgmonitor changes setup SQL to check if the
         # role exists prior to creating it.
         psql -U postgres --port="${PG_PRIMARY_PORT}" -d postgres \
-            < ${function_file?} > /tmp/pgmonitor.stdout 2> /tmp/pgmonitor.stderr
+            < "/tmp/setup_pg.sql" > /tmp/pgmonitor-setup.stdout 2> /tmp/pgmonitor-setup.stderr
 
         #err_check "$?" "pgMonitor Setup" "Could not load pgMonitor functions: \n$(cat /tmp/pgmonitor.stderr)"
 
         psql -U postgres --port="${PG_PRIMARY_PORT}" -d postgres \
             -c "ALTER ROLE ccp_monitoring PASSWORD '${PGMONITOR_PASSWORD?}'" \
-            > /tmp/pgmonitor.stdout 2> /tmp/pgmonitor.stderr
+            > /tmp/pgmonitor-alter-role.stdout 2> /tmp/pgmonitor-alter-role.stderr
 
         #err_check "$?" "pgMonitor User Setup" "Could not alter ccp_monitor user's password: \n$(cat /tmp/pgmonitor.stderr)"
     fi
