CrunchyData/crunchy-containers

commit 9404a8610c6e277279bebdd6f8b6caa7137f63c9

Add support for PostgreSQL 12.
This adds support for PostgreSQL 12 across all supported container platforms.

The biggest change in this release is the support for how PostgreSQL 12
handles standby/recovery mode. The "recovery.conf" file is no longer supported
by PostgreSQL. Instead, a user puts their standby/recovery settings into the
main "postgresql.conf" file, and indicates to the PostgreSQL instance what
mode to use based on the presence of a "standby.signal" or "recovery.signal"
file.

This accounts for this change, as well as enables the container builds across
the supported platforms.

Issue: [ch6258]

bin/postgres/start.sh
@@ -229,6 +229,70 @@ function initialize_replica() {
     fi
     echo_info "${APPLICATION_NAME} is the APPLICATION_NAME being used."
 
+    # PostgreSQL 12 changed how a replica/standby is set up. There is no longer
+    # a recovery.conf file that indicates a PostgreSQL instance is a standby,
+    # but rather one of two "signal" files that are available. The settings
+    # for the PostgreSQL recovery/standby are kept in the main PostgreSQL.conf
+    # file. As such we will fork off here, and allow each more to be st up.
+    PG_VERSION=`cat $PGDATA/PG_VERSION`
+    if [[ $PG_VERSION -ge 12 ]]; then
+        initialize_replica_post12
+    else
+        initialize_replica_pre12
+    fi
+}
+
+# PostgreSQL 12 moved all settings that affect a replica instance into the
+# main "postgresql.conf" file and removed the "replica.conf" file that would
+# put a PostgreSQL instance into recovery/standby mode. Now, in addition to the
+# recovery settings being in the "postgresql.conf" file, one must add one of the
+# following files in the $PGDATA directory to put the server into one of these
+# modes:
+#
+# - "standby.signal": used for a replica that you want in a current, read-only
+#                     state
+# - "recovery.signal": used for a "targeted recovery", e.g. a
+#                      point-in-time-recovery (PITR), which will stop replaying
+#                      logs once the targeted recovery point is reached.
+#
+# For more info:
+# https://www.postgresql.org/docs/current/runtime-config-wal.html#RUNTIME-CONFIG-WAL-ARCHIVE-RECOVERY
+function initialize_replica_post12() {
+    echo_info "Setting up recovery using methodology for PostgreSQL 12 and above."
+    # set up a temporary file with recovery settings that we output to
+    RECOVERY_FILE_TMP='/tmp/pgrepl-recovery.conf'
+    echo_info "Preparing recovery settings in ${RECOVERY_FILE_TMP}"
+    # first, create a temporary file to build up the recovery file separately.
+    # use the legacy name to help
+    touch $RECOVERY_FILE_TMP
+    # As of PostgreSQL 12, the recovery_target_timeline defaults to "latest",
+    # so we will not add that in.
+    #
+    # Also as of PostgreSQL 12, the "standby_mode" parameter is dropped, as that
+    # is now dictated by which signal file is used.
+    #
+    # In PostgreSQL 12, "trigger_file" => "promote_trigger_file"
+    echo "promote_trigger_file = '/tmp/pg-failover-trigger'" > $RECOVERY_FILE_TMP
+    # the primary_conninfo string stays mostly the same
+    PGCONF_PRIMARY_CONNINFO="application_name=${APPLICATION_NAME} host=${PG_PRIMARY_HOST} port=${PG_PRIMARY_PORT} user=${PG_PRIMARY_USER}"
+    echo "primary_conninfo = '${PGCONF_PRIMARY_CONNINFO}'" > $RECOVERY_FILE_TMP
+    # append the contents of $RECOVERY_FILE_TMP to the postgresql.conf file
+    cat $RECOVERY_FILE_TMP >> $PGDATA/postgresql.conf
+    # and put the server into standby mode
+    touch $PGDATA/standby.signal
+}
+
+# Before PostgreSQL 12, the way to set up a replica instance was to use a file
+# called recovery.conf. The recovery.conf file serves as a way to indicate to
+# PostgreSQL to boot up in "recovery" mode, and based on the settings, one could
+# create a "hot standby" where read-only queries could be routed. This is the
+# basis of streaming replication, etc. All the settings for setting up the file
+# are documented here:
+# https://www.postgresql.org/docs/11/recovery-config.html
+function initialize_replica_pre12() {
+    echo_info "Setting up recovery using methodology for PostgreSQL 11 and below."
+    # Basically, we have a preconfigured recovery file with some settings in it,
+    # And we substitute out some of the settings
     cp /opt/cpm/conf/pgrepl-recovery.conf /tmp
     sed -i "s/PG_PRIMARY_USER/$PG_PRIMARY_USER/g" /tmp/pgrepl-recovery.conf
     sed -i "s/PG_PRIMARY_HOST/$PG_PRIMARY_HOST/g" /tmp/pgrepl-recovery.conf

centos7/12/Dockerfile.backrest-restore.centos7
@@ -1,0 +1,61 @@
+FROM centos:7
+
+LABEL name="crunchydata/postgres" \
+        vendor="Crunchy Data" \
+        PostgresVersion="12" \
+        PostgresFullVersion="12.0" \
+        Version="7.7" \
+        Release="4.1.1" \
+        url="https://crunchydata.com" \
+        summary="Executes the pgbackrest utility, allowing FULL & DELTA restore capability." \
+        description="Executes pgbackrest utility, allowing FULL, DELTA & PITR restore capability. Capable of mounting the /backrestrepo for access to pgbackrest archives, while allowing for the configuration of pgbackrest using applicable pgbackrest environment variables." \
+        io.k8s.description="backrest restore container" \
+        io.k8s.display-name="Crunchy backrest restore container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm" PGDG_REPO_DISABLE="pgdg*" PGDG_REPO_ENABLE="pgdg12"\
+    BACKREST_VERSION="2.18"
+
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+RUN yum -y update && yum -y install epel-release \
+ && yum -y install  \
+	hostname \
+	gettext \
+	nss_wrapper \
+ 	procps-ng  \
+ && yum -y clean all
+
+RUN yum -y install postgresql12-server  \
+ && yum -y install --disablerepo="${PGDG_REPO_DISABLE}" --enablerepo="${PGDG_REPO_ENABLE}" pgbackrest-"${BACKREST_VERSION}" \
+ && yum -y clean all
+
+ENV	PGROOT="/usr/pgsql-${PGVERSION}"
+
+# add path settings for postgres user
+ADD conf/.bash_profile /var/lib/pgsql/
+
+# set up cpm directory
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf /pgdata /backrestrepo \
+	/var/lib/pgsql /var/log/pgbackrest
+
+RUN chown -R postgres:postgres /opt/cpm  \
+	/pgdata /backrestrepo  \
+	/var/lib/pgsql /var/log/pgbackrest
+
+# volume backrestrepo for pgbackrest to restore from and log
+VOLUME /pgdata /backrestrepo
+
+ADD bin/backrest_restore /opt/cpm/bin
+
+#removed to allow separate version of common_lib.sh for this container only. Its in
+# the bin/backrest_restore directory.
+#ADD bin/common /opt/cpm/bin
+
+ADD conf/backrest_restore /opt/cpm/conf
+
+USER 26
+CMD ["/opt/cpm/bin/start.sh"]

centos7/12/Dockerfile.backup.centos7
@@ -1,0 +1,50 @@
+FROM centos:7
+
+LABEL name="crunchydata/backup" \
+        vendor="Crunchy Data" \
+        PostgresVersion="12" \
+        PostgresFullVersion="12.0" \
+        Version="7.7" \
+        Release="4.1.1" \
+        url="https://crunchydata.com" \
+        summary="Performs a pg_basebackup full database backup on a database container" \
+        description="Meant to be executed upon demand, this container will run pg_basebackup against a running database container and write the backup files to a mounted directory." \
+        io.k8s.description="backup container" \
+        io.k8s.display-name="Crunchy backup container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm"
+
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+RUN yum -y update && yum install -y epel-release \
+ && yum -y update glibc-common \
+ && yum install -y bind-utils \
+	gettext \
+	hostname \
+	procps-ng \
+	unzip \
+ && yum -y install postgresql12 postgresql12-server \
+ && yum clean all -y
+
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf /pgdata
+ADD bin/backup/ /opt/cpm/bin
+ADD bin/common /opt/cpm/bin
+ADD conf/backup/ /opt/cpm/conf
+
+RUN chown -R postgres:postgres  /opt/cpm /pgdata && \
+        chmod -R g=u /opt/cpm /pgdata
+
+RUN chmod g=u /etc/passwd && \
+	chmod g=u /etc/group
+
+VOLUME ["/pgdata"]
+
+ENTRYPOINT ["opt/cpm/bin/uid_postgres.sh"]
+
+USER 26
+
+CMD ["/opt/cpm/bin/start-backupjob.sh"]

centos7/12/Dockerfile.collect.centos7
@@ -1,0 +1,55 @@
+FROM centos:7
+
+LABEL name="crunchydata/collect" \
+        vendor="Crunchy Data" \
+        PostgresVersion="12" \
+        PostgresFullVersion="12.0" \
+        Version="7.7" \
+        Release="4.1.1" \
+        url="https://crunchydata.com" \
+        summary="Provides metrics for crunchy-postgres" \
+        description="Run with crunchy-postgres, crunchy-collect reads the Postgres data directory and has a SQL interface to a database to allow for metrics collection. Used in conjunction with crunchy-prometheus and crunchy-grafana." \
+        io.k8s.description="collect container" \
+        io.k8s.display-name="Crunchy collect container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm"
+
+# Install the PGDG yum repo
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+# Install postgres client tools and libraries
+RUN yum install -y epel-release \
+  && yum -y update \
+  && yum -y install \
+    gettext \
+    postgresql12 \
+    postgresql12-libs \
+  && yum -y clean all
+
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf
+
+ADD postgres_exporter.tar.gz /opt/cpm/bin
+ADD tools/pgmonitor/exporter/postgres /opt/cpm/conf
+ADD bin/collect /opt/cpm/bin
+ADD bin/common /opt/cpm/bin
+ADD conf/collect /opt/cpm/conf
+
+RUN chgrp -R 0 /opt/cpm/bin /opt/cpm/conf && \
+    chmod -R g=u /opt/cpm/bin/ opt/cpm/conf
+
+# postgres_exporter
+EXPOSE 9187
+
+RUN chmod g=u /etc/passwd
+
+VOLUME ["/conf"]
+
+ENTRYPOINT ["/opt/cpm/bin/uid_daemon.sh"]
+
+USER 2
+
+CMD ["/opt/cpm/bin/start.sh"]

centos7/12/Dockerfile.pgadmin4.centos7
@@ -1,0 +1,63 @@
+FROM centos:7
+
+LABEL name="crunchydata/pgadmin4" \
+        vendor="Crunchy Data" \
+        PostgresVersion="12" \
+        PostgresFullVersion="12.0" \
+        Version="7.7" \
+        Release="4.1.1" \
+        url="https://crunchydata.com" \
+        summary="Crunchy Data pgAdmin4 GUI utility" \
+        description="Provides GUI for the pgAdmin utility." \
+        io.k8s.description="pgadmin4 container" \
+        io.k8s.display-name="Crunchy pgadmin4 container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm" PGDG_REPO_DISABLE="pgdg*" PGDG_REPO_ENABLE="pgdg12"
+
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+RUN yum -y update && yum -y install epel-release \
+ && yum -y install glibc-common-*2.17* \
+ && yum -y install gcc \
+    gettext \
+    hostname \
+    openssl \
+    procps-ng \
+    mod_wsgi mod_ssl \
+ && yum -y install --disablerepo="${PGDG_REPO_DISABLE}" --enablerepo="${PGDG_REPO_ENABLE}" postgresql12-devel postgresql12-server pgadmin4-web \
+ && yum -y clean all
+
+ENV PGROOT="/usr/pgsql-${PGVERSION}"
+
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf /var/lib/pgadmin /certs /run/httpd
+
+ADD bin/pgadmin4/ /opt/cpm/bin
+ADD bin/common /opt/cpm/bin
+ADD conf/pgadmin4/ /opt/cpm/conf
+
+RUN cp /opt/cpm/conf/httpd.conf /etc/httpd/conf/httpd.conf \
+  && rm /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/ssl.conf
+
+RUN chown -R 2:0 /usr/lib/python2.7/site-packages/pgadmin4-web \
+   /var/lib/pgadmin /certs /etc/httpd /run/httpd /var/log/httpd && \
+    chmod -R g=u /usr/lib/python2.7/site-packages/pgadmin4-web \
+   /var/lib/pgadmin /certs /etc/httpd /run/httpd /var/log/httpd
+
+RUN ln -sf /var/lib/pgadmin/config_local.py /usr/lib/python2.7/site-packages/pgadmin4-web/config_local.py \
+  && ln -sf /var/lib/pgadmin/pgadmin.conf /etc/httpd/conf.d/pgadmin.conf
+
+EXPOSE 5050
+
+RUN chmod g=u /etc/passwd
+
+VOLUME ["/var/lib/pgadmin", "/certs", "/run/httpd"]
+
+ENTRYPOINT ["opt/cpm/bin/uid_daemon.sh"]
+
+USER 2
+
+CMD ["/opt/cpm/bin/start-pgadmin4.sh"]

centos7/12/Dockerfile.pgbadger.centos7
@@ -1,0 +1,55 @@
+FROM centos:7
+
+LABEL name="crunchydata/pgbadger" \
+        vendor="Crunchy Data" \
+        PostgresVersion="12" \
+        PostgresFullVersion="12.0" \
+        Version="7.7" \
+        Release="4.1.1" \
+        url="https://crunchydata.com" \
+        summary="HTTP wrapper around the PGBadger PostgreSQL utility" \
+        description="Has an HTTP REST interface. You GET http://host:10000/api/badgergenerate, and it will generate a pgbadger report on a database container's log files." \
+        io.k8s.description="pgbadger container" \
+        io.k8s.display-name="Crunchy pgbadger container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm" PGDG_REPO_DISABLE="pgdg*" PGDG_REPO_ENABLE="pgdg12"
+
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+RUN yum -y update \
+ && yum -y install epel-release \
+ && yum -y install --disablerepo="${PGDG_REPO_DISABLE}" --enablerepo="${PGDG_REPO_ENABLE}"\
+      gettext \
+      hostname \
+      pgbadger \
+ && yum clean all -y
+
+RUN groupadd -g 26 postgres && useradd -g 26 -u 26 postgres
+
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf /report
+
+ADD conf/pgbadger /opt/cpm/conf
+ADD bin/common /opt/cpm/bin
+ADD bin/pgbadger /opt/cpm/bin
+
+RUN chown -R postgres:postgres /opt/cpm /report /bin && \
+        chmod -R g=u /opt/cpm /report /bin
+
+# pgbadger port
+EXPOSE 10000
+
+RUN chmod g=u /etc/passwd && \
+	chmod g=u /etc/group
+
+VOLUME ["/pgdata", "/report"]
+
+ENTRYPOINT ["opt/cpm/bin/uid_postgres.sh"]
+
+
+USER 26
+
+CMD ["/opt/cpm/bin/start-pgbadger.sh"]

centos7/12/Dockerfile.pgbench.centos7
@@ -1,0 +1,54 @@
+FROM centos:7
+
+LABEL name="crunchydata/pgbench" \
+        vendor="Crunchy Data" \
+        PostgresVersion="12" \
+        PostgresFullVersion="12.0" \
+        Version="7.7" \
+        Release="4.1.1" \
+        url="https://crunchydata.com" \
+    	summary="pgBench 12 (PGDG) on a Centos7 base image" \
+        description="pgbench is a simple program for running benchmark tests on PostgreSQL. It runs the same sequence of SQL commands over and over, possibly in multiple concurrent database sessions, and then calculates the average transaction rate (transactions per second)." \
+        io.k8s.description="pgbench container" \
+        io.k8s.display-name="Crunchy pgbench container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm"
+
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+RUN yum -y update \
+ && yum -y install epel-release \
+ && yum -y update glibc-common \
+ && yum -y install bind-utils \
+    gettext \
+    hostname \
+    procps-ng  \
+    rsync \
+ && yum -y install postgresql12 \
+ && yum -y clean all
+
+ENV PGROOT="/usr/pgsql-${PGVERSION}"
+
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf
+
+RUN chown -R 26:0 /opt/cpm \
+ && chmod -R g=u /opt/cpm
+
+ADD bin/pgbench /opt/cpm/bin
+ADD bin/common /opt/cpm/bin
+ADD conf/pgbench /opt/cpm/conf
+
+RUN chmod g=u /etc/passwd && \
+	chmod g=u /etc/group
+
+ENTRYPOINT ["/opt/cpm/bin/uid_postgres.sh"]
+
+VOLUME ["/pgconf"]
+
+USER 26
+
+CMD ["/opt/cpm/bin/start.sh"]

centos7/12/Dockerfile.pgbouncer.centos7
@@ -1,0 +1,52 @@
+FROM centos:7
+
+LABEL name="crunchydata/pgbouncer" \
+		vendor="Crunchy Data" \
+		PostgresVersion="12" \
+		PostgresFullVersion="12.0" \
+		Version="7.7" \
+		Release="4.1.1" \
+        url="https://crunchydata.com" \
+        summary="Lightweight connection pooler for crunchy-postgres" \
+        description="The aim of crunchy-pgbouncer is to lower the performance impact of opening new connections to PostgreSQL; clients connect through this service. It offers session, transaction and statement pooling." \
+        io.k8s.description="pgbouncer container" \
+        io.k8s.display-name="Crunchy pgbouncer container" \
+        io.openshift.expose-services="" \
+        io.openshift.tags="crunchy,database"
+
+COPY licenses /licenses
+
+ENV PGVERSION="12" PGDG_REPO="pgdg-redhat-repo-latest.noarch.rpm" PGDG_REPO_DISABLE="pgdg*" PGDG_REPO_ENABLE="pgdg12"
+
+RUN rpm -Uvh https://download.postgresql.org/pub/repos/yum/${PGVERSION}/redhat/rhel-7-x86_64/${PGDG_REPO}
+
+RUN yum -y update \
+ && yum -y install epel-release \
+ && yum -y install --disablerepo="${PGDG_REPO_DISABLE}" --enablerepo="${PGDG_REPO_ENABLE}" \
+      gettext \
+      hostname  \
+      pgbouncer \
+      postgresql12 \
+      procps-ng \
+ && yum clean all -y
+
+RUN mkdir -p /opt/cpm/bin /opt/cpm/conf /pgconf
+
+ADD bin/pgbouncer /opt/cpm/bin
+ADD bin/common /opt/cpm/bin
+ADD conf/pgbouncer /opt/cpm/conf
+
+RUN chown -R 2:0 /opt/cpm /pgconf && \
+    chmod -R g=u /opt/cpm /pgconf
+
+EXPOSE 6432
+
+RUN chmod g=u /etc/passwd
+
+VOLUME ["/pgconf"]
+
+ENTRYPOINT ["opt/cpm/bin/uid_daemon.sh"]
+
+USER 2
+
+CMD ["/opt/cpm/bin/start.sh"]

commit 1610046f8c959284836749b9091d545ff0c234f9

Fix for PostgreSQL 12 recovery settings being overwritten.
Commit 9404a861 introduced support for PostgreSQL 12, but also
included a bug where the "promote_trigger_file" GUC was being
immediately overwritten after being set.

This fix ensures that the "promote_trigger_file" GUC remains
after being set.

Issue: #1164

bin/postgres/start.sh
@@ -275,7 +275,7 @@ function initialize_replica_post12() {
     echo "promote_trigger_file = '/tmp/pg-failover-trigger'" > $RECOVERY_FILE_TMP
     # the primary_conninfo string stays mostly the same
     PGCONF_PRIMARY_CONNINFO="application_name=${APPLICATION_NAME} host=${PG_PRIMARY_HOST} port=${PG_PRIMARY_PORT} user=${PG_PRIMARY_USER}"
-    echo "primary_conninfo = '${PGCONF_PRIMARY_CONNINFO}'" > $RECOVERY_FILE_TMP
+    echo "primary_conninfo = '${PGCONF_PRIMARY_CONNINFO}'" >> $RECOVERY_FILE_TMP
     # append the contents of $RECOVERY_FILE_TMP to the postgresql.conf file
     cat $RECOVERY_FILE_TMP >> $PGDATA/postgresql.conf
     # and put the server into standby mode