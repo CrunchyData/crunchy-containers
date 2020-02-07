14c970105b04a4d98f6446c3c4d90aac9082dfbd
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2020-02-06 23:42:36 -0500

    Create pgbouncer helpers functions as part of container initialization
    The PostgreSQL Operator would provide some embedded SQL to help with
    this process, but arguably was doing too much work. The requirements
    it has for using pgBouncer are fairly straightforward:
    
    - Have a pgBouncer user
    - Have a pgBouncer schema created in all accessible databases
    - Have a "get_auth" function that follows the "auth_query" pattern
    defined in the pgBouncer docs[1]
    - Lock everything down as much as possible regarding what the pgbouncer
    user can do
    
    This commit provides the scripts to initialize the pgbouncer user
    and machinery in the PostgreSQL HA container, but defers to the
    container user to determine if they get executed.
    
    [1] http://www.pgbouncer.org/config.html#auth_query

---
+++ bin/postgres-ha/sql/pgbouncer/pgbouncer-uninstall.sql
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2016 - 2020 Crunchy Data Solutions, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * pgbouncer-uninstall.sql removes the general infrastructure for using pgBouncer
+ * with the PostgreSQL Operator
+ *
+ * This has to be executed in every database that had pgbouncer made available
+ * to as well as template1
+ */
+
+/**
+ * Remove the SECURITY DEFINER function that returns non-privileged and
+ * non-system user credentials
+ */
+DROP FUNCTION IF EXISTS pgbouncer.get_auth(username TEXT);
+
+/**
+ * Drop the "pgbouncer" schema, and if anything exists in it, ensure it is
+ * wiped out. Woe to those who used a system schema to store their own things...
+ */
+DROP SCHEMA IF EXISTS pgbouncer CASCADE;
+
+/**
+ * Drop anything owned by the pgbouncer user. It should be nothing at this
+ * point, but better safe than sorry...
+ */
+DROP OWNED BY pgbouncer CASCADE;
+
+/**
+ * So, we can't drop the pgbouncer role as this file runs on an individual
+ * database. We need **all** objects associated "pgbouncer" dropped before we
+ * can execute `DROP ROLE pgbouncer;`
+ */
