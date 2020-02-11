c735ff5c948d158c379dd0479edc3d774406a513
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2020-02-10 19:40:38 -0500

    Update Copyright to 2020
    This also hits a few files with various formatters, which is why there
    are a few extra changes.

--- tools/test-harness/custom_config_ssl_test.go
+++ tools/test-harness/custom_config_ssl_test.go
@@ -1,5 +1,20 @@
 package tests
 
+/*
+Copyright 2018 - 2020 Crunchy Data Solutions, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import (
 	"fmt"
 	"os"
@@ -55,16 +70,16 @@ func TestCustomConfigSSL(t *testing.T) {
 		t.Fatalf("extensions less then 1, it shouldn't be: %d", len(extensions))
 	}
 
-    settings, err := db.Settings()
-    if err != nil {
-        t.Fatal(err)
-    }
+	settings, err := db.Settings()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    for _, setting := range settings {
-        if setting.Name == "log_timezone" && setting.Value != "UTC" {
-            t.Fatalf("log_timezone isn't UTC, it should be: %s = %s", setting.Name, setting.Value)
-        }
-    }
+	for _, setting := range settings {
+		if setting.Name == "log_timezone" && setting.Value != "UTC" {
+			t.Fatalf("log_timezone isn't UTC, it should be: %s = %s", setting.Name, setting.Value)
+		}
+	}
 
 	report, err := harness.createReport()
 	if err != nil {
