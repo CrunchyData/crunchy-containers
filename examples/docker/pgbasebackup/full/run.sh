ee1c3047eaf815a3f9bd5b7304476fc85732d89c
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2020-02-10 19:40:38 -0500

    Update Copyright to 2020
    This also hits a few files with various formatters, which is why there
    are a few extra changes.

--- examples/docker/pgbasebackup/full/run.sh
+++ examples/docker/pgbasebackup/full/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2016 - 2019 Crunchy Data Solutions, Inc.
+# Copyright 2016 - 2020 Crunchy Data Solutions, Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -28,7 +28,7 @@ echo "Starting the pg_basebackup full restore example..."
 docker run \
 	--volume full-restore-pgdata:/pgdata \
 	--volume backup-volume:/backup:ro \
-	--env BACKUP_PATH=primary-backups/2019-05-08-21-07-31 \
+	--env BACKUP_PATH=primary-backups/2020-02-08-21-07-31 \
 	--env PGDATA_PATH=pgbasebackup-full-restored \
 	--name="${CONTAINER_NAME}" \
 	--hostname="${CONTAINER_NAME}" \
--- examples/kube/pgaudit/configs/pgaudit-test.sql
+++ examples/kube/pgaudit/configs/pgaudit-test.sql
@@ -1,2 +1,17 @@
+/*
+ * Copyright 2018 - 2020 Crunchy Data Solutions, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 DROP TABLE IF EXISTS t_random;
 CREATE TABLE t_random AS SELECT s, md5(random()::text) FROM generate_series(1,50) s;
