9310b7c4988582db399b7b5f22554da560e061e8
Francesco Frassinelli <fraph24@gmail.com>
2020-02-28 16:59:08 +0100

    conf/pgpool/pgpool.conf: replace sed with envsubst

--- bin/pgpool/startpgpool.sh
+++ bin/pgpool/startpgpool.sh
@@ -48,10 +48,7 @@ else
     env_check_err "PG_PASSWORD"
 
     # populate template with env vars
-    sed -i "s/PG_PRIMARY_SERVICE_NAME/${PG_PRIMARY_SERVICE_NAME?}/g" ${CONFIGS?}/pgpool.conf
-    sed -i "s/PG_REPLICA_SERVICE_NAME/${PG_REPLICA_SERVICE_NAME?}/g" ${CONFIGS?}/pgpool.conf
-    sed -i "s/PG_USERNAME/${PG_USERNAME?}/g" ${CONFIGS?}/pgpool.conf
-    sed -i "s/PG_PASSWORD/${PG_PASSWORD?}/g" ${CONFIGS?}/pgpool.conf
+    envsubst < ${CONF_DIR?}/pgpool.conf > ${CONFIGS?}/pgpool.conf
 
     echo_info "Populating pool_passwd.."
     /bin/pg_md5 --md5auth --username=${PG_USERNAME?} --config=${CONFIGS?}/pgpool.conf ${PG_PASSWORD?}
