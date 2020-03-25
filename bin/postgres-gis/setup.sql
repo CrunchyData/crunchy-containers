7a07d1a1b7903c7ea76d231669e8f7df34005263
Chris Bandy <chris.bandy@crunchydata.com>
2020-03-25 09:29:52 -0500

    Quote identifiers in setup SQL
    In the Operator, the database name can default to the cluster name which
    can have dashes and full stops.

--- bin/postgres-gis/setup.sql
+++ bin/postgres-gis/setup.sql
@@ -10,17 +10,17 @@ create extension plr;
 
 alter user postgres password 'PG_ROOT_PASSWORD';
 
-create user PG_PRIMARY_USER with REPLICATION  PASSWORD 'PG_PRIMARY_PASSWORD';
+create user "PG_PRIMARY_USER" with REPLICATION  PASSWORD 'PG_PRIMARY_PASSWORD';
 create user "PG_USER" with password 'PG_PASSWORD';
 
 create table primarytable (key varchar(20), value varchar(20));
-grant all on primarytable to PG_PRIMARY_USER;
+grant all on primarytable to "PG_PRIMARY_USER";
 
-create database PG_DATABASE;
+create database "PG_DATABASE";
 
-grant all privileges on database PG_DATABASE to "PG_USER";
+grant all privileges on database "PG_DATABASE" to "PG_USER";
 
-\c PG_DATABASE
+\c "PG_DATABASE"
 
 create extension postgis;
 create extension postgis_topology;
@@ -30,7 +30,7 @@ create extension pg_stat_statements;
 create extension pgaudit;
 create extension plr;
 
-\c PG_DATABASE "PG_USER";
+\c "PG_DATABASE" "PG_USER";
 
 create schema "PG_USER";
 
@@ -45,4 +45,4 @@ create table "PG_USER".testtable (
 insert into "PG_USER".testtable (name, value, updatedt) values ('CPU', '256', now());
 insert into "PG_USER".testtable (name, value, updatedt) values ('MEM', '512m', now());
 
-grant all on "PG_USER".testtable to PG_PRIMARY_USER;
+grant all on "PG_USER".testtable to "PG_PRIMARY_USER";
