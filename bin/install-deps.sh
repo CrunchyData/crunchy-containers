d5ce396ec59a8fc07f100e2cd38a3e25af60465d
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2020-04-23 19:11:32 +0000

    Update yq to Version 3.3.0
    This commit updates the version of 'yq' to v3.3.0 in order to leverage
    the compare feature as needed for cluster configuration enhancements in
    the PostgreSQL Operator.

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -20,7 +20,7 @@ POSTGRES_EXPORTER_VERSION=0.7.0
 PGMONITOR_COMMIT='v3.2'
 OPENSHIFT_CLIENT='https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz'
 CERTSTRAP_VERSION=1.1.1
-YQ_VERSION=2.4.0
+YQ_VERSION=3.3.0
 
 sudo yum -y install net-tools bind-utils wget unzip git
 
