17b47fb1941f4af944550f5c906b5ce1a4b35a7c
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2020-06-08 09:08:49 -0400

    Bump Grafana to v6.7.4

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -15,7 +15,7 @@
 
 # Dependency Versions
 PROMETHEUS_VERSION=2.9.2
-GRAFANA_VERSION=6.3.4
+GRAFANA_VERSION=6.7.4
 POSTGRES_EXPORTER_VERSION=0.4.7
 PGMONITOR_COMMIT='v3.2'
 OPENSHIFT_CLIENT='https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz'
