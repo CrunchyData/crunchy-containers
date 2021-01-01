added0f9d0766bf24ad57b1220c2528bb7853024
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2021-01-01 14:10:38 -0500

    Bump pgMonitor to v4.4 tag

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Dependency Versions
-PGMONITOR_COMMIT='v4.4-RC7'
+PGMONITOR_COMMIT='v4.4'
 OPENSHIFT_CLIENT='https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz'
 CERTSTRAP_VERSION=1.1.1
 YQ_VERSION=3.3.0
